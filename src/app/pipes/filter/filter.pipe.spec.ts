d5d91c5934f0904c03ed994df3a0682e69aafc61
gattlin1 <gattlin1@live.missouristate.edu>
2020-11-07 18:02:25 -0500

    Fixed filter pipe to handle objects instead of arrays. Also updated filter test to make sure it is working properly.

--- src/app/pipes/filter/filter.pipe.spec.ts
+++ src/app/pipes/filter/filter.pipe.spec.ts
@@ -1,8 +1,32 @@
+import { Pokemon } from 'src/models/pokemon.model';
 import { FilterPipe } from './filter.pipe';
 
 describe('FilterPipe', () => {
   it('create an instance', () => {
     const pipe = new FilterPipe();
     expect(pipe).toBeTruthy();
+  });
+
+  it('Filters Pokemon', () => {
+    // arrange
+    const pipe = new FilterPipe();
+    const searchText = 'c';
+    const data = {
+      a: new Pokemon(),
+      ab: new Pokemon(),
+      b: new Pokemon(),
+      c: new Pokemon()
+    };
+    const expected = {
+      c: new Pokemon()
+    };
+
+    // act
+    const actual = pipe.transform(data, searchText);
+    const keys = Object.keys(actual);
+
+    // assert
+    expect(keys.length).toBe(1);
+    expect(keys[0]).toBe('c');
   });
 });
--- src/app/pipes/filter/filter.pipe.ts
+++ src/app/pipes/filter/filter.pipe.ts
@@ -1,18 +1,23 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { PokemonContainer } from 'src/models/pokemon-container.model';
 import { Pokemon } from '../../../models/pokemon.model';
 
 @Pipe({
   name: 'filter'
 })
 export class FilterPipe implements PipeTransform {
-  transform(items: Array<Pokemon>, searchText: string): Array<Pokemon> {
-    if (!items) { return []; }
+  transform(items: any, searchText: string): PokemonContainer {
+    if (!items) { return {}; }
     if (!searchText) { return items; }
 
+    const results: PokemonContainer = {};
     searchText = searchText.toLowerCase();
-    return items.filter( (pokemon: Pokemon) => {
-      return pokemon.name.toLowerCase().includes(searchText);
-    });
-  }
 
+    for (const name of Object.keys(items)) {
+      if (name.toLowerCase().includes(searchText)) {
+        results[name] = items[name];
+      }
+    }
+    return results;
+  }
 }
